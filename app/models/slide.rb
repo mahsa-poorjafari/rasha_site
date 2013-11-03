672033d2f66da948234693a30e5f36fe074b9385
mahsa <m_mw7462@yahoo.com>
2013-11-03 12:09:30 +0330

    set crop on slide/edit

--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,4 +1,4 @@
 class Slide < ActiveRecord::Base
   attr_accessible :description, :image
-  has_attached_file :image, :styles => { :small => "150x150>", :large => "500X600" }
+  has_attached_file :image, :styles => { :small => "150x150>", :large => "950X360" }
 end
