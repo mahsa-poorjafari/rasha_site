ef6ca541a6701c810ef266d69d7d4a4c9903e1e4
mohamamd <mb.sepanta@gmail.com>
2013-11-05 14:35:02 +0330

    added  fa fields  to products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ActiveRecord::Base
   has_many :pictures, :dependent => :destroy
-  attr_accessible :description, :title, :pictures_attributes
+  attr_accessible :description, :title, :description_fa, :title_fa, :pictures_attributes
   accepts_nested_attributes_for :pictures
-  
+ 
   validates :title, :uniqueness => true
   validates :title, :presence => {:message => 'The product title must be informed.'}
+
 end
