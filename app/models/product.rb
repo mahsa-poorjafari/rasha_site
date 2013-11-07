32f3e2f9ecf916c1a32b49b9058e4abfac932734
mohamamd <mb.sepanta@gmail.com>
2013-11-07 09:29:44 +0330

    added fa version for prodcut table

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,10 +4,12 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :pictures
  
   validates :title, :uniqueness => true
-  
   validates :title, :presence => {:message => 'The product title must be informed.'}
 
-  extend FriendlyId  
-  friendly_id :title
+  def title
+    I18n.locale == :fa ? self.read_attribute("title_fa") : self.read_attribute("title")
+  end
+  def description
+    I18n.locale == :fa ? self.read_attribute("description_fa") : self.read_attribute("description")
+  end
 end
-
