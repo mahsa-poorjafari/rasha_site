a4814cc8e08142fb553fd9242e4ebed3c4b341fb
mohamamd <mb.sepanta@gmail.com>
2013-11-07 09:38:00 +0330

    put back freindly_id for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,10 +5,13 @@ class Product < ActiveRecord::Base
  
   validates :title, :uniqueness => true
   validates :title, :presence => {:message => 'The product title must be informed.'}
+  extend FriendlyId  
+  friendly_id :title
 
   def title
     I18n.locale == :fa ? self.read_attribute("title_fa") : self.read_attribute("title")
   end
+  
   def description
     I18n.locale == :fa ? self.read_attribute("description_fa") : self.read_attribute("description")
   end
