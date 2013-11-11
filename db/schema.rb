c29e4a1af8a927e2beaf8be77d007b12f1d3ddd8
mahsa <m_mw7462@yahoo.com>
2013-11-11 09:37:35 +0330

    modify part 12

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 # It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20131109060828) do
+
   create_table "ckeditor_assets", :force => true do |t|
     t.string   "data_file_name",                  :null => false
     t.string   "data_content_type"
