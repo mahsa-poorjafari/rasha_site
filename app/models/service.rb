c645f98610dd6dd74c07cc413e300a128119cfb5
mahsa <m_mw7462@yahoo.com>
2013-11-17 15:32:46 +0330

    set partial upload image form

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,5 +1,6 @@
 class Service < ActiveRecord::Base
   has_many :pictures, :dependent => :destroy
+  
   attr_accessible :description, :title, :pictures_attributes, :description_fa, :title_fa
   accepts_nested_attributes_for :pictures, :allow_destroy => true
   
@@ -13,6 +14,10 @@ class Service < ActiveRecord::Base
   end
   def description
     I18n.locale == :fa ? self.read_attribute("description_fa") : self.read_attribute("description")
+  end
+  
+  def sample_picture
+    Picture.where(service_id:self.id).first
   end
   
 end
