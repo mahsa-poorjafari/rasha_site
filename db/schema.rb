9eb9aa6ed5efa7564c4c7cddae663515a5d9b5ad
mahsa <m_mw7462@yahoo.com>
2013-11-30 17:17:36 +0330

    fix fadeIn & fadeOut for customer logo description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131128091954) do
+ActiveRecord::Schema.define(:version => 20131130084950) do
 
   create_table "customers", :force => true do |t|
     t.string   "name"
