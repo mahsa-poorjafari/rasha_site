7cece04290aa719bac6954912304cc9f47a5c30e
mahsa <m_mw7462@yahoo.com>
2014-01-06 13:45:19 +0330

    delete product title of home page

--- Rakefile
+++ Rakefile
@@ -4,4 +4,4 @@
 
 require File.expand_path('../config/application', __FILE__)
 
-IndustiralSite::Application.load_tasks
+Rashasmart::Application.load_tasks
--- db/migrate/20131127132745_create_services.rb
+++ db/migrate/20131127132745_create_services.rb
@@ -6,6 +6,10 @@ class CreateServices < ActiveRecord::Migration
       t.integer  "sample_pic_id"
       t.datetime "created_at",     :null => false
       t.datetime "updated_at",     :null => false   
+      t.string   "pdf_file_name"
+      t.string   "pdf_content_type"
+      t.integer  "pdf_file_size"
+      t.datetime "pdf_updated_at"
     end
   end
   
