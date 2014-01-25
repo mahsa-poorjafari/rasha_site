1e234303b56f431b85326949adb3c2a540212a1a
mohamamd <mb.sepanta@gmail.com>
2014-01-25 16:02:59 +0330

    migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140125092426) do
+ActiveRecord::Schema.define(:version => 20140125075114) do
 
   create_table "associates", :force => true do |t|
     t.string   "name"
@@ -106,7 +106,7 @@ ActiveRecord::Schema.define(:version => 20140125092426) do
     t.string   "password"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
-    t.text     "email"
+    t.string   "email"
   end
 
 end
